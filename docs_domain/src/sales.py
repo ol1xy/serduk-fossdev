00e8a2393cd1d9be754917ea4340c4c7681bfd47
ol1xy <aleksejserduk1.@gmail.com>
2026-04-02 13:07:47 +0800

    Added value check into sale parcing

--- docs_domain/src/sales.py
+++ docs_domain/src/sales.py
@@ -8,8 +8,13 @@ def _parse_sale_record(line: str):
 
     product_name = sale[0] 
     category = sale[1]  
-    unit_price = float(sale[2]) 
-    quantity = int(sale[3]) 
+    try:
+        unit_price = float(sale[2]) 
+        quantity = int(sale[3]) 
+        if int(sale[3]) != float(sale[3]):
+            return None
+    except ValueError:
+        return None
 
     return {"n": product_name, "c": category, "a": unit_price, "q": quantity}
 
